a2d2f8f8c2bfd8aaa4f380ed151a766cc243f0fe
Lynx3d <lynx3d@some-imaginary-isp.org>
2010-07-07 11:10:32 +0200

    More log output...
    Really final code cleanup i hope.

--- src/shared/vmap/VMapDefinitions.h
+++ src/shared/vmap/VMapDefinitions.h
@@ -43,12 +43,13 @@ namespace VMAP
 #ifndef NO_CORE_FUNCS
     #include "Errors.h"
     #include "Log.h"
-    #define ERROR_LOG(...) sLog.outDebug(__VA_ARGS__);
+    #define ERROR_LOG(...) sLog.outError(__VA_ARGS__);
 #else
-    #define ASSERT(x) do{} while(0)
-    #define DEBUG_LOG(...) do{} while(0)
-    #define ERROR_LOG(...) do{} while(0)
-    #define DETAIL_LOG(...) do{} while(0)
+    #include <assert.h>
+    #define ASSERT(x) assert(x)
+    #define DEBUG_LOG(...) do{ printf(__VA_ARGS__); printf("\n"); } while(0)
+    #define DETAIL_LOG(...) do{ printf(__VA_ARGS__); printf("\n"); } while(0)
+    #define ERROR_LOG(...) do{ printf("ERROR:"); printf(__VA_ARGS__); printf("\n"); } while(0)
 #endif
 
 #endif // _VMAPDEFINITIONS_H
